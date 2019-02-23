fa0275c44a65f70db1b904e59de55f4f079775b6
tsn150197 <famaxlevel1997@gmail.com>
2019-02-23 11:12:34 +0700

    Admin's home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     base_title = t ".daily_report"
     page_title.empty? ? base_title : page_title + " | " + base_title
   end
+
+  def current_user
+    return unless user_id = session[:user_id]
+    @current_user ||= User.find_by id: user_id
+  end
 end
