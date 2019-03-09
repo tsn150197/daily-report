c925030ca20428e3f2171f1d1c833503de981641
tsn150197 <famaxlevel1997@gmail.com>
2019-03-09 14:07:03 +0700

    Current user profile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 //= require AdminatorByColorlib/vendor
 //= require AdminatorByColorlib/bundle
 //= require jquery
-//= require rails-ujs
+//= require jquery_ujs
 //= require ckeditor/init
 //= require activestorage
 //= require cable
