11f76b3205f4c51d446b9d00ab4134253b7708da
tsn150197 <famaxlevel1997@gmail.com>
2019-03-14 16:09:21 +0700

    Data validation

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,10 +2,26 @@ class Report < ApplicationRecord
   enum status: {rejected: 0, approved: 1}
   belongs_to :user
   has_many :comments, dependent: :destroy
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: {scope: :user_id},
+    length: {maximum: Settings.string_maximum}
   validates :date, presence: true
+  validate :min_date, :max_date
   scope :order_date, ->{order date: :desc}
   scope :where_title, ->(title){where "title LIKE ?", "%#{title}%"}
   scope :where_from_date, ->(from_date){where "date >= ?", from_date}
   scope :where_to_date, ->(to_date){where "date <= ?", to_date}
+
+  private
+
+  def min_date
+    return unless date <= Settings.min_date
+    errors.add :date, I18n.t("activerecord.min_date",
+      date: Settings.min_date.strftime("%d/%m/%Y"))
+  end
+
+  def max_date
+    return unless date >= Settings.max_date
+    errors.add :date, I18n.t("activerecord.max_date",
+      date: Settings.max_date.strftime("%d/%m/%Y"))
+  end
 end
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,6 +1,27 @@
 class UserProfile < ApplicationRecord
   enum gender: {female: 0, male: 1}
   belongs_to :user
-  validates :name, presence: true, length: {maximum: Settings.name_maximum}
+  validates :name, presence: true, length: {maximum: Settings.name_maximum},
+    uniqueness: true
+  validates :address, length: {maximum: Settings.string_maximum},
+    allow_nil: true
+  validates :phone, length: {maximum: Settings.string_maximum},
+    allow_blank: true,
+      format: {with: /\A\d+\z/, message: I18n.t("activerecord.number")}
+  validate :min_birthday, :max_birthday
   mount_uploader :avatar_url, PictureUploader
+
+  private
+
+  def min_birthday
+    return unless birthday <= Settings.min_date
+    errors.add :birthday, I18n.t("activerecord.min_date",
+      date: Settings.min_date.strftime("%d/%m/%Y"))
+  end
+
+  def max_birthday
+    return unless birthday >= Settings.max_date
+    errors.add :birthday, I18n.t("activerecord.max_date",
+      date: Settings.max_date.strftime("%d/%m/%Y"))
+  end
 end
